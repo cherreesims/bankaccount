9e2ba7adb02132da631025673f598589ac7d37f0
cherreesims <145623229+cherreesims@users.noreply.github.com>
2023-09-24 18:34:04 -0500

    Update Bankaccount.java

--- Bankaccount.java
+++ Bankaccount.java
@@ -19,3 +19,36 @@ public void withdrawl(double amount) {
         this.balance -= amount;
     }
 }
+
+public void setFirstName(String firstName) {
+  this.firstName = firstName;
+}
+
+public String getFirstName() {
+    return firstName;
+}
+
+public void setLastName(String lastName) {
+  this.lastName = lastName;
+}
+
+public String getLastName() {
+    return lastName;
+}
+
+public void setAccountID(int accountID) {
+    this.accountID = accountID;
+}
+
+public double getBalance() {
+    reutrn balance;
+}
+
+public void accountSummary() {
+  System.out.println("Account Information:");
+  System.out.println("First Name: " + firstName);
+  System.out.println("Last Name: " + lastName);
+  System.out.println("Account ID: " + accountID);
+  System.out.println("Balance: $" + balance);
+}
+}
