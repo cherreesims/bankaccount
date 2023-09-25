c75979420e123906d2d0187e09be0bda17a557fa
cherreesims <145623229+cherreesims@users.noreply.github.com>
2023-09-24 21:15:12 -0500

    Create checkingaccount.java

---
+++ checkingaccount.java
@@ -0,0 +1,28 @@
+public class Checking Account extends BankAccount {
+  private double interestRate;
+
+  public CheckingAccount() {
+    super();
+    this.interestRate = 0.0;
+  }
+
+public void processWithdrawl(double amount) {
+  if (amount > geBalance()) {
+       System.out.println("Warning: Insufficient funds. $30 overdraft fee charged.");
+       super.withdrawl(30.0);
+  }
+  super.withdrawl(amount);
+}
+
+public void displayAccount() {
+  super.accountSummary();
+  System.out.println("Interest Rate: " interestRate);
+}
+
+public void setInterestRate(double interestRate) {
+  this.interestRate = interestRate;
+}
+  public double getInterestRate() {
+    return interestRate;
+  }
+}
